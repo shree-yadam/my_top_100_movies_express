660dfcd78e5bfbed9a87787bae3795b64755ae42
Shree Devi Yadam <shree.yadam@gmail.com>
2022-07-14 23:59:02 -0700

    Add register and encrypt password

--- db/queries/appUserQueries.js
+++ db/queries/appUserQueries.js
@@ -1,7 +1,17 @@
 const getUserByEmail = (db, email) => {
   const queryStr = "SELECT * FROM app_user WHERE email = $1";
   const queryParams = [email];
-  console.log("Query");
+  return db.query(queryStr, queryParams)
+    .then((data) => {
+      return data.rows[0];
+    });
+}
+
+const addUser = (db, user) => {
+  const queryStr = `INSERT INTO app_user (name, email, password)
+                    VALUES ($1, $2, $3)
+                    RETURNING *`;
+  const queryParams = [user.name, user.email, user.password];
   return db.query(queryStr, queryParams)
     .then((data) => {
       return data.rows[0];
@@ -9,5 +19,6 @@ const getUserByEmail = (db, email) => {
 }
 
 module.exports = {
-  getUserByEmail
+  getUserByEmail,
+  addUser
 }
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,20 @@
-const { Console } = require("console");
 const express = require("express");
 const router = express.Router();
 const appUserDbHelper = require("../db/queries/appUserQueries");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = (db) => {
 
   router.post("/login", (req, res) => {
-    console.log("In Login");
     const {email, password} = req.body;
     appUserDbHelper.getUserByEmail(db, email)
     .then(user => {
-      if(user.password == password) {
-        console.log(`Logged in as ${user.name}`);
-        res.send(`Logged in as ${user.name}`);
+      if(!user || !bcrypt.compareSync(password, user.password)) {
+        res.status(401).send("Invalid Login");
         return;
       }
-      res.send("Invalid Login");
+      res.send(`Logged in as ${user.name}`);
     })
     .catch(err => {
       console.log(err);
@@ -24,7 +23,32 @@ module.exports = (db) => {
   });
 
   router.post("/register", (req, res) => {
-    res.send("Register");
+    const user = req.body;
+    if(!(user.email || user.password || user.name)) {
+      res.status(400).send("All fields required");
+      return;
+    }
+
+    appUserDbHelper.getUserByEmail(db, user.email)
+    .then(existingUser => {
+      if(existingUser) {
+        res.status(400).send("email already in use!");
+        return;
+      }
+      user.password = bcrypt.hashSync(user.password, saltRounds);
+      return appUserDbHelper.addUser(db, user);
+    })
+    .then(addedUser => {
+      if(!addedUser) {
+        res.status(500).send("Error adding User");
+        return;
+      }
+      res.send("User Logged in");
+    })
+    .catch(err => {
+      console.log("Err");
+      res.status(500).send("Error");
+    });
   });
 
   return router;
