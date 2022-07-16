6e2b0908e00c0300f17ac5af97c9442cf326e8a3
Shree Devi Yadam <shree.yadam@gmail.com>
2022-07-15 23:43:05 -0700

    Add cors

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const logger = require("morgan");
 const userRouter = require("./routes/users");
 require("dotenv").config();
 const db = require("./db/index");
+const cors = require("cors");
 
 const PORT = 8080;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger("dev"));
-
+app.use(cors());
 app.use("/users", userRouter(db));
 
 app.get("/", (req, res) => {
