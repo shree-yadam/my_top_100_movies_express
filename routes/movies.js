88c6ab5376c58b0765f67c9c77dc3f29705d9934
Shree Devi Yadam <shree.yadam@gmail.com>
2022-07-20 23:48:49 -0700

    just send required information for list of movies

--- routes/movies.js
+++ routes/movies.js
@@ -4,12 +4,24 @@ const axios = require("axios");
 
 module.exports = () => {
 
-  router.get("/", (req, res) => {
-    let url = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.API_KEY_v3_AUTH}`;
+  let base_url = "";
+  let url = `https://api.themoviedb.org/3/configuration?api_key=${process.env.API_KEY_v3_AUTH}`;
     axios.get(url)
     .then(response => {
-      console.log(response.data);
-      res.json(response.data);
+      base_url = response.data.images.base_url;
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  router.get("/", (req, res) => {
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY_v3_AUTH}`;
+    axios.get(url)
+    .then(response => {
+      let movieData = [...(response.data.results)];
+      let resData = movieData.map(m => {
+        return {title: m.title, description: m.overview, img_url: `${base_url}original${m.poster_path}`};
+      });
+      res.json(resData);
     })
     .catch(err => {
       console.log(err);
